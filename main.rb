1ecace19585530b2cd31ef340b89953355e71936
Giovanecunhadias <98927748+Giovanecunhadias@users.noreply.github.com>
2024-02-09 21:45:13 -0400

    Add files via upload

---
+++ main.rb
@@ -0,0 +1,12 @@
+#nome
+print "Dgite seu nome: "
+nome = gets.chomp
+#sobrenome
+print "Digite seu sobrenome: "
+sobre_nome = gets.chomp
+#idade
+print "Digite sua idade: "
+idade = gets.chomp
+ #saída de dados
+
+puts"Nome: #{nome}, Sobrenome: #{sobre_nome}, Idade: #{idade}"
